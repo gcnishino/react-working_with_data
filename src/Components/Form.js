3a7a6eb130d4836d4a18ca669ab142e8cc7f0b79
sr1994lu <sr1994lu@gmail.com>
2017-07-09 05:20:56 +0900

    build(src): Form ref

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,12 +2,17 @@ import React, {Component} from 'react'
 import Card from './Card'
 
 class Form extends Component {
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log('Event: From Submit', this.userNameInput.value)
+  }
+
   render() {
     return (
-      <div className="Form">
-        <input type="text" placeholder="Github username" />
+      <form className="Form" onSubmit={this.handleSubmit}>
+        <input type="text" ref={input => (this.userNameInput = input)} placeholder="Github username" required />
         <button type="submit">Add card</button>
-      </div>
+      </form>
     )
   }
 }
